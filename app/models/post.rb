8c36d2d39ed172fa016b6a3320c5bfe3a3cedd5e
Aleksandar Ivezic <a.ivezic@yahoo.com>
2021-06-01 17:58:12 +0200

    Add Comment model and associations and validations related to it

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,5 +3,5 @@ class Post < ApplicationRecord
     validates :body, presence: true, length: {minimum: 10}
 
     belongs_to :user
-    
+    has_many :comments
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
     validates :password, presence: true, length: {in: 6..50}
 
     has_many :posts
+    has_many :comments
 end
