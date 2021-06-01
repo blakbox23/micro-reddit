1a821d3866d9da09ce9bb77705ca617b79a3ce6d
Aleksandar Ivezic <a.ivezic@yahoo.com>
2021-06-01 16:09:15 +0200

    add validations to a User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+    validates :username, presence: true, uniqueness: true, length: { minimum: 3 , message: "%{count} characters is the minimum allowed "}
+    validates :email, presence: true, uniqueness:true, length: {in: 6..50}
+    validates :password, presence: true, length: {in: 6..50}
 end
