6e9a398b57bfc58a32c038d77303100304f2263a
Aleksandar Ivezic <a.ivezic@yahoo.com>
2021-06-02 11:47:12 +0200

    Fix rubocop errors

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,5 @@
-# frozen_string_literal: true
-
 class Post < ApplicationRecord
-  validates :title, presence: true, length: { minimum: 3, message: '%{count} characters is the minimum allowed ' }
+  validates :title, presence: true, length: { minimum: 3, message: '%<count>s characters is the minimum allowed ' }
   validates :body, presence: true, length: { minimum: 10 }
 
   belongs_to :user
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,6 @@
-# frozen_string_literal: true
-
 class User < ApplicationRecord
   validates :username, presence: true, uniqueness: true,
-                       length: { minimum: 3, message: '%{count} characters is the minimum allowed ' }
+                       length: { minimum: 3, message: '%<count>s characters is the minimum allowed ' }
   validates :email, presence: true, uniqueness: true, length: { in: 6..50 }
   validates :password, presence: true, length: { in: 6..50 }
 
